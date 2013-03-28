0bb09ac6bb4d0d25f68fdc069dd74c78fa69649d
shen.zhang <shen.zhang@dianping.com>
2013-03-28 17:19:58 +0800

    Initial Commit

---
+++ DrawerFrame/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DrawerFrame
+//
+//  Created by Johnson Zhang on 13-3-28.
+//  Copyright (c) 2013年 Theosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DrawerFrame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DrawerFrame
+//
+//  Created by Johnson Zhang on 13-3-28.
+//  Copyright (c) 2013年 Theosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
